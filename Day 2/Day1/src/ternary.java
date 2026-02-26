f561769a7190ebfcfcc26a053c26d475163c25e8
arunkumar45 <arunkumarar58@gmail.com>
2026-02-26 22:12:21 +0530

    Removed duplicate Day 2

--- Day 2/Day1/src/ternary.java
+++
@@ -1,9 +0,0 @@
-public class ternary{
-    public static void main(String[] args){
-        String v= (2>3)? "True":"False";
-        char x=(2>3)?'T':'F';
-        int i=(2<3)?1:0;
-        System.out.println(i);
-
-    }
-}
